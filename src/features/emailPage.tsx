7851adb3594dec715e052e0029fbcf8c2316b775
Ravi kumar <1002kumarravi@gmail.com>
2023-01-16 01:48:53 +0530

    single email view

--- src/features/emailPage.tsx
+++ src/features/emailPage.tsx
@@ -1,9 +1,10 @@
 import { EmailCard } from "../components/email-card/EmailCard";
+import { EmailDetailsOpen } from "../components/emailDetailsOpen/EmailDetailsOpen";
 import { useAppSelector } from "../hooks/reduxHooks";
 import "./email-page.css";
 
 export const EmailPage = () => {
-  const { emails } = useAppSelector((state) => state.emailsList);
+  const { emails, emailData } = useAppSelector((state) => state.emailsList);
 
   return (
     <div>
@@ -12,6 +13,9 @@ export const EmailPage = () => {
           emails.map((email) => {
             return <EmailCard emailData={email} key={email.id} />;
           })}
+      </div>
+      <div>
+        <EmailDetailsOpen />
       </div>
     </div>
   );
